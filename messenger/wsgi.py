c087495a92191b31f8648b21374bb345b766445a
codact <savalijey@gmail.com>
2019-09-04 20:23:36 +0300

    Switch location for auto seed countries

--- messenger/wsgi.py
+++ messenger/wsgi.py
@@ -1,17 +1,8 @@
 
 import os
-from django_countries import countries
-from messenger.apps.country.models import Country
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
-
-try:
-    for code, name in list(countries):
-        Country.objects.get_or_create(name=name, code=code)
-except BaseException:
-    pass
-
 
 application = get_wsgi_application()
