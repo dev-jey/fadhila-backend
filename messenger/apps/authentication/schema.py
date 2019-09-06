f1c6efd081f7046533136245a1d876d79c69ffc0
codact <savalijey@gmail.com>
2019-09-06 17:38:53 +0300

    Fix updte profile bug

--- messenger/apps/authentication/schema.py
+++ messenger/apps/authentication/schema.py
@@ -209,6 +209,7 @@ class UpdateProfile(graphene.Mutation):
             user.save()
             return UpdateProfile(user=user)
         except Exception as error:
+            print(error)
             raise GraphQLError('There has been an error updating your profile.' 
             +' Try again later')
 
